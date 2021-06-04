4f77f641a9c45077e4937f82e16b6940cff537f5
nadiahareb <nadia.hareb.ensi@gmail.com>
2021-06-04 10:56:58 -0400

    une composatnte qui liste les composantes de selfie

--- 001/MaPremiereApplication/src/app/app.module.ts
+++ 001/MaPremiereApplication/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuGeneralComponent } from './shared/components/menu-general/menu-general.component';
 import { SelfieListComponent } from './features/selfies/selfie-list/selfie-list.component';
+import { UnSelfieReadonlyComponent } from './features/selfies/un-selfie-readonly/un-selfie-readonly.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuGeneralComponent,
-    SelfieListComponent
+    SelfieListComponent,
+    UnSelfieReadonlyComponent
   ],
   imports: [
     BrowserModule
